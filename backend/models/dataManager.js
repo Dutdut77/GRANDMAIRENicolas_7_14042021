fcab26930c18751b030621db54d535fc17e9e44e
Nicolas GRANDMAIRE <dutdut77@hotmail.fr>
2021-04-26 22:25:12 +0200

    Gestion des erreurs

---
+++ backend/models/dataManager.js
@@ -0,0 +1,33 @@
+
+const mariadb = require('mariadb');
+let conn;
+
+const pool = mariadb.createPool({ host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE });
+
+
+module.exports.start  = async function() {
+    conn = await pool.getConnection();
+}
+
+
+module.exports.addUser = async function(sql, data = []){
+    const res = await conn.query(sql, data);
+    delete res["meta"];
+    return res;
+}
+
+
+module.exports.findAll = async function(sql, data = []){
+    const res = await conn.query(sql, data);
+    delete res["meta"];
+    return res;
+}
+
+module.exports.findOne = async function(sql, data = []) {
+    const res = await conn.query(sql, data);
+    delete res["meta"];
+    return res[0];
+}
+
+
+
