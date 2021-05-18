30c32ae95d305921ae147e1fc8d6895c17c99c66
Nicolas GRANDMAIRE <dutdut77@hotmail.fr>
2021-05-18 23:50:45 +0200

    Login frontend

---
+++ groupomania/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: function () {
+      return import(/* webpackChunkName: "about" */ "../views/About.vue");
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: function () {
+      return import(/* webpackChunkName: "login" */ "../views/Login.vue");
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
