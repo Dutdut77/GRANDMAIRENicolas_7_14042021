f119a95edabfb281d4330618c1f9552c69306f72
Nicolas GRANDMAIRE <dutdut77@hotmail.fr>
2021-06-24 23:39:06 +0200

    Page Album rectif affichage photo

--- groupomania/src/components/Card2.vue
+++ groupomania/src/components/Card2.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="card"  @click="OneStorie(storie.id)">
-  <img src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
-  <p class="title">{{ storie.pseudo }}</p>
-  <p class="date">{{ storie.date }}</p>
-  <div class="overlay"></div>
-  <div class="button"><img src="../assets/plus.png" class="plus" /></div>
-</div>
+  <div class="card" @click="OneStorie(storie.id)">
+    <img
+      src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg="
+      alt=""
+    />
+    <p class="title">{{ storie.pseudo }}</p>
+    <p class="date">{{ storie.date }}</p>
+    <div class="overlay"></div>
+    <div class="button"><img src="../assets/plus.png" class="plus" /></div>
+  </div>
 </template>
 
 <script>
@@ -15,7 +18,7 @@ export default {
     storie: {
       default: "Valeur par défaut",
     },
-  },  
+  },
   methods: {
     OneStorie(id) {
       this.$router.push({ name: "OneStorie", params: { id: id } });
@@ -25,19 +28,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
 $primary: #091f43;
 $secondary: #d1515a;
 
-
 .card {
   position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
   margin-top: 20px;
-  width: 100%;
-  height: 300px;
-  border-radius : 4px;
+  border-radius: 4px;
   box-shadow: 0 5px 20px rgba(9, 31, 67, 0.5);
   overflow: hidden;
+  cursor: pointer;
   @media (min-width: 768px) {
     width: 30%;
   }
@@ -60,10 +64,10 @@ $secondary: #d1515a;
 }
 
 img {
-  position: absolute;
- height: 100%;
+  position: relative;
+  width: 100%;
   left: 0;
-  border-radius : 4px;
+  border-radius: 4px;
 }
 
 .title {
@@ -73,12 +77,12 @@ img {
   bottom: 5px;
   font-weight: 400;
   font-size: 1.5rem;
-  text-align: left;  
-  padding-left:  10px;
+  text-align: left;
+  padding-left: 10px;
   color: white;
   z-index: 1;
   opacity: 1;
-  transition: opacity .4s ease;
+  transition: opacity 0.4s ease;
 }
 
 .card:hover .title {
@@ -93,49 +97,30 @@ img {
   font-size: 1rem;
   font-style: italic;
   text-align: right;
-  padding-right:  10px;
+  padding-right: 10px;
   color: white;
-  z-index: 1;
   opacity: 1;
-  transition: opacity .5s ease;
+  transition: opacity 0.5s ease;
 }
 
 .card:hover .date {
   opacity: 0;
 }
 
-.button {
+.button {  
   position: absolute;
-  width: 100%;
-  cursor: pointer;
-  top: 130px;
-  text-align: center;
+   margin : auto auto;
   opacity: 0;
-  transition: opacity .35s ease;
-  
+  transition: opacity 0.35s ease;
 }
 
-.button a {
-outline: none;
-  text-decoration: none;
-  width: 200px;
-  padding: 6px 12px;
-  text-align: center;
-  color: $primary;
-  font-weight: 700;
-  font-size : 30px;
-  border: solid 2px white;
-  background-color: white;
-  border-radius : 100%;
-  z-index: 1;
-}
 .plus {
-  position: relative;
-  width:36px;
+
+
+  width: 36px;
   height: 36px;
 }
 .card:hover .button {
   opacity: 1;
 }
-
 </style>
